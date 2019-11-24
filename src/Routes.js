543dcac4cddeba00a087dcb20c7642a698c32b8d
Marjan Mukram <marjanmukram8@gmail.com>
2019-11-25 01:03:01 +0530

    Routes basic setup

--- src/Routes.js
+++ src/Routes.js
@@ -1,2 +1,14 @@
 //Switch case
 //Import to app.js
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+const Routes = () => (
+  <div>
+    <Switch>
+      <Route exact path="/customers" />
+    </Switch>
+  </div>
+);
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import configureStore from "./store";
-import './assets/fontello/css/animation.css';
-import './assets/fontello/css/fontello.css';
+import "./assets/fontello/css/animation.css";
+import "./assets/fontello/css/fontello.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
