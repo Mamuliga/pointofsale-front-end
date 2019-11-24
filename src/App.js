6a96c86f4bcdfca361f9ba15ff0d519426cbb5ee
Marjan Mukram <marjanmukram8@gmail.com>
2019-11-24 17:41:14 +0530

    Material ui removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { simpleAction } from "./store/actions/simpleAction";
-import MaterialUIButtons from "./components/uis/materialUiButton";
 import VectorIcon from "./components/uis/VectorIcon";
 import { heart, pause, play } from "./assets/vectorIcons";
 import { DatePicker } from 'antd';
@@ -15,7 +14,6 @@ function App(props) {
       </pre>
       <header className="App-header">
         EIP POS FRONT END
-        <MaterialUIButtons />
         <VectorIcon name={heart} /> <VectorIcon name={play} />
         <VectorIcon name={pause} />
         <button onClick={() => props.simpleAction()}>Click me</button>
