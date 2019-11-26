b914c262ed58aa764247d13f56b7e647bf6ee4c0
Marjan Mukram <marjanmukram8@gmail.com>
2019-11-26 21:35:26 +0530

    Log in page added

---
+++ src/components/login/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => (
+  <div>
+    <h1>This is Login page</h1>
+    Click here to login
+    <button>Log in</button>
+  </div>
+);
+
+export default Login;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
 //Switch case
 //Import to app.js
-import React from 'react';
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Main from '../components/main';
-import Customers from '../components/customers';
-import Suppliers from '../components/suppliers';
-import Sales from '../components/sales';
+import Main from "../components/main";
+import Customers from "../components/customers";
+import Suppliers from "../components/suppliers";
+import Sales from "../components/sales";
+import Login from "../components/login";
 
 const Routes = () => (
   <div>
     <Switch>
       <Route exact path="/" component={Main} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/customers" component={Customers} />
       <Route exact path="/suppliers" component={Suppliers} />
       <Route exact path="/sales" component={Sales} />
