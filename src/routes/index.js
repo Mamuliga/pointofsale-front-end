3c812d30d79669ec566a131fee36063d3105a72b
Marjan Mukram <marjanmukram8@gmail.com>
2019-11-26 21:24:08 +0530

    Basic roues added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,18 @@
 //Import to app.js
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
+import Main from '../components/main';
+import Customers from '../components/customers';
+import Suppliers from '../components/suppliers';
+import Sales from '../components/sales';
 
 const Routes = () => (
   <div>
     <Switch>
-      <Route exact path="/customers" />
+      <Route exact path="/" component={Main} />
+      <Route exact path="/customers" component={Customers} />
+      <Route exact path="/suppliers" component={Suppliers} />
+      <Route exact path="/sales" component={Sales} />
     </Switch>
   </div>
 );
