d5cbbd8df7878faede597c015bde461a61c9f172
rimaz.emeraldit <rimaz.emeraldit@gmail.com>
2019-11-27 01:06:43 +0530

    login finished 80%

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { Button } from 'antd'
+import ReactDOM from 'react-dom';
+import { Icon, Input, Button, Form } from 'antd';
+import './login.css';
+
+
 
 const LoginPage = () => {
-    return (<div>
-        <h1>THis is login LoginPage</h1>
-        <Button>Log in</Button>
-        <Button>submit</Button>
-    </div>)
+    return (
+        <Form className="login-form">
+            <h1 className="center">Welcome</h1>
+            <Form.Item label="UserName">
+                <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Username"
+                />
+            </Form.Item>
+
+            <Form.Item label="Password">
+                <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Button type="primary" block>
+                Login
+            </Button>
+            <a className="login-form-forgot`" href="">
+                Forgot password
+          </a>
+
+        </Form>
+    );
 }
 export default LoginPage;
