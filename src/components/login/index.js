643e8f32eb400baa19d371a410fdec334db4d2e2
Marjan Mukram <marjanmukram8@gmail.com>
2019-11-26 22:24:15 +0530

    Basic login logout hanlded

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { authenticate, logout } from "../../store/actions/authActions";
+import {Button} from 'antd'
 
-const Login = () => (
+const Login = ({ authReducer, authenticate, logout  }) => (
   <div>
     <h1>This is Login page</h1>
-    Click here to login
-    <button>Log in</button>
+    <pre>{JSON.stringify(authReducer)}</pre>
+    <h2>Click here to login/logut</h2>
+    <Button type="primary" onClick={authReducer && authReducer.isAuthenticated?logout:authenticate}>
+      {authReducer && authReducer.isAuthenticated ? "Log out" : "Log in"}
+    </Button>
   </div>
 );
 
-export default Login;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  authenticate: () => dispatch(authenticate()),
+  logout: () => dispatch(logout())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
