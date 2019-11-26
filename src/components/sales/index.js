cc1d2327635d597a81fedc36c3b30d403bd6c883
Marjan Mukram <marjanmukram8@gmail.com>
2019-11-26 21:11:23 +0530

    Sample pages added

---
+++ src/components/sales/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Sales = () => <h1>Sales</h1>;
+
+export default Sales;
---
+++ src/components/suppliers/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Suppliers = () => 
+<h1>Suppliers</h1>
+
+export default Suppliers;
