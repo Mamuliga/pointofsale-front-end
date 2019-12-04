3660fa59a35d6ba337f56b46a58fe4d79fca859b
Marjan Mukram <marjanmukram8@gmail.com>
2019-12-04 23:22:53 +0530

    Customer row selection handled

--- src/components/pages/Customers.js
+++ src/components/pages/Customers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "antd";
 import mockCustomers from "../../utilities/mockData/customers.json";
 import { getCustomerTableHeaders } from "../../utilities/helpers/tableHelpers";
@@ -9,14 +9,40 @@ const Customers = () => {
       ...customer
     }));
   };
-
   const customerRowKey = customer => `${customer.username}`;
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+  const selectRow = record => {
+    if (selectedRowKeys.indexOf(record.key) >= 0) {
+      selectedRowKeys.splice(selectedRowKeys.indexOf(record.key), 1);
+    } else {
+      selectedRowKeys.push(record.key);
+    }
+    setSelectedRowKeys(selectedRowKeys);
+  };
+
+  const handleChangeRowSelection = selectedRowKeys => {
+    setSelectedRowKeys(selectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: handleChangeRowSelection
+  };
+
+  const handleOnRow = record => ({
+    onClick: selectRow(record)
+  });
+
+  console.log(selectedRowKeys);
 
   return (
     <Table
       columns={getCustomerTableHeaders()}
       dataSource={customerTableContent()}
       rowKey={customerRowKey}
+      rowSelection={rowSelection}
+      onRow={handleOnRow}
     />
   );
 };
