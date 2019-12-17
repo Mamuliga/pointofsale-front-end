327d25177b34482cd1d4f501403b393e4acd2f63
Marjan Mukram <marjanmukram8@gmail.com>
2019-12-17 22:40:11 +0530

    typeof clearFilters === "function"

--- src/components/uis/CustomizedTable.js
+++ src/components/uis/CustomizedTable.js
@@ -27,7 +27,9 @@ const CustomizedTable = ({
   };
 
   const handleReset = clearFilters => {
-    clearFilters();
+    if (typeof clearFilters === "function") {
+      clearFilters();
+    }
     setSearchedText("");
   };
 
