94102f2a7c7b1d608cb97d06ae461bb68de5c10f
Marjan Mukram <marjanmukram8@gmail.com>
2020-01-12 21:20:57 +0530

    Material ui initial setup

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <Button variant='contained' color='primary'>
+        Button from material ui
+      </Button>
     </div>
   );
 };
