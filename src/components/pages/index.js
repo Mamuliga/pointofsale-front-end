80d5de0b3491bb87bf2f566282ec97b3b220f5a4
Marjan Mukram <marjanmukram8@gmail.com>
2020-02-15 13:37:16 +0530

    ./Form issue

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,6 +2,5 @@ import Customers from "./Customers";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import NotFoundPage from "./NotFoundPage";
-import Form from "./Form";
 
-export { Dashboard, Login, Customers, NotFoundPage, Form };
+export { Dashboard, Login, Customers, NotFoundPage };
