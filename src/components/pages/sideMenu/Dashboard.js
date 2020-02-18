9ff7fde6ea537077cae65e15a7777ae27a51ef2a
Marjan Mukram <marjanmukram8@gmail.com>
2020-02-18 23:10:46 +0530

    Route url bugs fixed

--- src/components/pages/sideMenu/Dashboard.js
+++ src/components/pages/sideMenu/Dashboard.js
@@ -1,21 +1,22 @@
-import React, { Fragment } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { useHistory } from "react-router-dom";
 import {
   Divider,
   ListItem,
   ListItemIcon,
   ListItemText
-} from '@material-ui/core';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
+} from "@material-ui/core";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import { PAGE_ROUTES } from "../../../services/routeService";
 
 const Dashboard = props => {
-  const { location, push } = useHistory();
+  const { push } = useHistory();
   return (
     <Fragment>
       <Divider />
       <ListItem
         onClick={e => {
-          push(`${location.pathname}/${e.key || 'summery'}`);
+          push(`${PAGE_ROUTES.home}/${e.key || "summery"}`);
         }}
         button
       >
