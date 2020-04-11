adb159605cc9b0c30271269b51312678256483ed
Marjan Mukram <marjanmukram8@gmail.com>
2020-04-11 15:08:23 +0530

    Dashboard imported twice handled

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
-import { Dashboard, Login, Customers, NotFoundPage } from '../components/pages';
-import ProtectedRoute from './ProtectedRoute';
-import { PAGE_ROUTES } from '../services/routeService';
+import React from "react";
+import { Switch } from "react-router-dom";
 import {
   Dashboard,
   Login,
   Customers,
-  Items,
   NotFoundPage,
-} from '../components/pages';
-import ProtectedRoute from './ProtectedRoute';
-import { PAGE_ROUTES } from '../services/routeService';
+  Items
+} from "../components/pages";
+import ProtectedRoute from "./ProtectedRoute";
+import { PAGE_ROUTES } from "../services/routeService";
 import {
   customerRoutes,
   employeeRoutes,
   supplierRoutes,
   itemRoutes,
   cashupRoutes,
-  saleRoutes,
-} from './routeHelper';
+  saleRoutes
+} from "./routeHelper";
 
-const Routes = (props) => (
+const Routes = props => (
   <Switch>
     <ProtectedRoute exact path={PAGE_ROUTES.home} component={Dashboard} />
     <ProtectedRoute
@@ -30,7 +27,7 @@ const Routes = (props) => (
       component={Login}
       authRequired={false}
     />
-    {customerRoutes.map((route) => (
+    {customerRoutes.map(route => (
       <ProtectedRoute
         exact
         path={route.path}
@@ -38,7 +35,7 @@ const Routes = (props) => (
         key={route.path}
       />
     ))}
-    {employeeRoutes.map((route) => (
+    {employeeRoutes.map(route => (
       <ProtectedRoute
         exact
         path={route.path}
@@ -46,7 +43,7 @@ const Routes = (props) => (
         key={route.path}
       />
     ))}
-    {supplierRoutes.map((route) => (
+    {supplierRoutes.map(route => (
       <ProtectedRoute
         exact
         path={route.path}
@@ -54,7 +51,7 @@ const Routes = (props) => (
         key={route.path}
       />
     ))}
-    {itemRoutes.map((route) => (
+    {itemRoutes.map(route => (
       <ProtectedRoute
         exact
         path={route.path}
@@ -62,7 +59,7 @@ const Routes = (props) => (
         key={route.path}
       />
     ))}
-    {cashupRoutes.map((route) => (
+    {cashupRoutes.map(route => (
       <ProtectedRoute
         exact
         path={route.path}
@@ -70,7 +67,7 @@ const Routes = (props) => (
         key={route.path}
       />
     ))}
-    {saleRoutes.map((route) => (
+    {saleRoutes.map(route => (
       <ProtectedRoute
         exact
         path={route.path}
