06e9f28a3168c0b8a7618b27b5e446bdbb4cb8d3
Marjan Mukram <marjanmukram8@gmail.com>
2020-04-24 22:37:45 +0530

    Node env bug fixed

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,6 @@ const devApiUrl = "http://eit-pos-api.herokuapp.com/api/";
 const prodApiUrl = "https://eit-pos-api.herokuapp.com/api/";
 
 export default {
-  apiUrl: process.env === "production" ? prodApiUrl : devApiUrl,
+  apiUrl: process.env.NODE_ENV === "production" ? prodApiUrl : devApiUrl,
   ...process.env
 };
