00010a1a61a2316ad4fb59407eb2bf9cb9593805
rimaz.emeraldit <rimaz.emeraldit@gmail.com>
2020-05-03 18:06:36 +0530

    apibinding-items-fixed

--- src/components/pages/Items.js
+++ src/components/pages/Items.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
-import TableBuilder from "../uis/TableBuilder.js";
-import { useHistory } from "react-router-dom";
-import { getItemTableHeaders } from "../../utilities/helpers/tableHelpers.js";
-import { getItemList } from "../../http/itemApi";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import TableBuilder from '../uis/TableBuilder.js';
+import { useHistory } from 'react-router-dom';
+import { getItemTableHeaders } from '../../utilities/helpers/tableHelpers.js';
+import { getItemList } from '../../http/itemApi';
+import { fetchApi, setFetchApiErr } from '../../store/actions/globalAction.js';
 
-const Items = () => {
+const Items = ({ fetchApi, setFetchApiErr }) => {
   const { location, push } = useHistory();
   const [itemList, setItemList] = useState([]);
 
   useEffect(() => {
     const handleGetItemResp = res => {
+      fetchApi(false);
       if (Array.isArray(res.data)) {
         const displayItemList = res.data.map(
           ({ id, barcode, itemName, category, reOrderLevel }) => {
@@ -19,12 +22,16 @@ const Items = () => {
         setItemList(displayItemList);
       }
     };
-    const handleGetItemErr = () => {};
+    const handleGetItemErr = () => {
+      setFetchApiErr('Unable to get items');
+      fetchApi(false);
+    };
 
+    fetchApi(true);
     getItemList()
       .then(handleGetItemResp)
       .catch(handleGetItemErr);
-  }, []);
+  }, [fetchApi, setFetchApiErr]);
 
   const handleEdit = item => {
     const editClick = () => {
@@ -36,10 +43,18 @@ const Items = () => {
     <TableBuilder
       tableData={itemList}
       tableHeaders={getItemTableHeaders}
-      title={"Items"}
+      title={'Items'}
       handleEdit={handleEdit}
     />
   );
 };
+const mapStateToProps = ({ global }) => {
+  return { ...global };
+};
 
-export default Items;
+const mapActionToProps = {
+  fetchApi,
+  setFetchApiErr
+};
+
+export default connect(mapStateToProps, mapActionToProps)(Items);
