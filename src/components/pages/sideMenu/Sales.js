eb7a474d4dc14c8599da64fb5bb1fa8fa41f930e
Marjan Mukram <marjanmukram8@gmail.com>
2020-05-11 21:35:38 +0530

    Side menu initials

--- src/components/pages/sideMenu/Sales.js
+++ src/components/pages/sideMenu/Sales.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import useStyles from '../../../styles/useStyles';
 import Button from '@material-ui/core/Button';
@@ -6,50 +7,53 @@ import Barcode from 'react-barcode';
 
 const Sale = props => {
   const classes = useStyles();
+  console.log(props);
 
   return (
     <Fragment>
-      <div>
-        <div className={classes.Id}>
-          <TextField id='sale-id' label='Sale Id' />
-        </div>
+      <form>
         <div className={classes.customerName}>
           <TextField id='sale-customer-name' label='Customer Name' />
         </div>
-        <div className={classes.total}>
-          <TextField id='sale-total-inputs' label='Total' />
-        </div>
-
-        <div className={classes.cash}>
-          <TextField id='sale-cash-inputs' label='Cash' />
-        </div>
-        <div className={classes.balance}>
-          <TextField id='sale-balance-inputs' label='Balance' />
-        </div>
-        <div>
-          <Button
-            className={classes.button}
-            variant='contained'
-            color='primary'
-          >
-            Submit
-          </Button>
-        </div>
-        <div>
-          <Button
-            className={classes.button}
-            variant='contained'
-            color='secondary'
-          >
-            Discard
-          </Button>
-          <div className={classes.barcode}>
-            <Barcode value='0000000000001' />
+        <div style={{ position: 'fixed', bottom: 0 }}>
+          <div className={classes.total}>
+            <TextField id='sale-total-inputs' label='Total' />
+          </div>
+          <div className={classes.cash}>
+            <TextField id='sale-cash-inputs' label='Cash' />
+          </div>
+          <div className={classes.balance}>
+            <TextField id='sale-balance-inputs' label='Balance' />
+          </div>
+          <div>
+            <Button
+              className={classes.button}
+              variant='contained'
+              color='primary'
+            >
+              Submit
+            </Button>
+          </div>
+          <div>
+            <Button
+              className={classes.button}
+              variant='contained'
+              color='secondary'
+            >
+              Discard
+            </Button>
+            <div className={classes.barcode}>
+              <Barcode value='0000000000001' />
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </Fragment>
   );
 };
 
-export default Sale;
+const mapStateToProps = ({ global }) => ({ ...global });
+
+const mapActionToProps = {};
+
+export default connect(mapStateToProps, mapActionToProps)(Sale);
