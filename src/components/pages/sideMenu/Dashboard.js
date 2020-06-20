7ae153c510ae5a9f79ba2fb2659af444be4f5d4a
Marjan Mukram <marjanmukram8@gmail.com>
2020-06-20 22:06:03 +0530

    Side menus  listings added

--- src/components/pages/sideMenu/Dashboard.js
+++ src/components/pages/sideMenu/Dashboard.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
-import { useHistory } from "react-router-dom";
+import React, { Fragment } from 'react';
+import { useHistory } from 'react-router-dom';
 import {
   Divider,
   ListItem,
   ListItemIcon,
-  ListItemText
-} from "@material-ui/core";
-import MenuBookIcon from "@material-ui/icons/MenuBook";
-import { PAGE_ROUTES } from "../../../services/routeService";
+  ListItemText,
+} from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import { PAGE_ROUTES } from '../../../services/routeService';
 
 const Dashboard = props => {
   const { push } = useHistory();
@@ -16,14 +16,50 @@ const Dashboard = props => {
       <Divider />
       <ListItem
         onClick={e => {
-          push(`${PAGE_ROUTES.home}/${e.key || "summery"}`);
+          push(`${PAGE_ROUTES.home}/${e.key || 'summary'}`);
         }}
         button
       >
         <ListItemIcon>
           <MenuBookIcon />
         </ListItemIcon>
-        <ListItemText>Summery</ListItemText>
+        <ListItemText>Summary</ListItemText>
+      </ListItem>
+      <Divider />
+      <ListItem
+        onClick={e => {
+          push(`${PAGE_ROUTES.home}/${'dailySales'}`);
+        }}
+        button
+      >
+        <ListItemIcon>
+          <MenuBookIcon />
+        </ListItemIcon>
+        <ListItemText>Daily Sales</ListItemText>
+      </ListItem>
+      <Divider />
+      <ListItem
+        onClick={e => {
+          push(`${PAGE_ROUTES.home}/${'mostSelledItems'}`);
+        }}
+        button
+      >
+        <ListItemIcon>
+          <MenuBookIcon />
+        </ListItemIcon>
+        <ListItemText>Most Selled Items</ListItemText>
+      </ListItem>
+      <Divider />
+      <ListItem
+        onClick={e => {
+          push(`${PAGE_ROUTES.home}/${'bestCustomers'}`);
+        }}
+        button
+      >
+        <ListItemIcon>
+          <MenuBookIcon />
+        </ListItemIcon>
+        <ListItemText>Best Customers</ListItemText>
       </ListItem>
       <Divider />
     </Fragment>
