4fe0369c69be2ca86a8eb6a60b893a111a530ca2
Marjan Mukram <marjanmukram8@gmail.com>
2020-06-30 23:33:30 +0530

    Customerlist api

--- src/components/pages/Customers.js
+++ src/components/pages/Customers.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { connect } from 'react-redux';
 import TableBuilder from '../uis/TableBuilder.js';
 import { useHistory } from 'react-router-dom';
 import { getCustomerTableHeaders } from '../../utilities/helpers/tableHelpers.js';
-import { getCustomerList } from '../../http/customerApi';
+import { getCustomerList, searchCustomer } from '../../http/customerApi';
 import { fetchApi, setFetchApiInfo } from '../../store/actions/globalAction.js';
+import useStyles from '../../styles/useStyles.js';
+import { TextField } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const Customers = ({ fetchApi, setFetchApiInfo }) => {
   const { location, push } = useHistory();
   const [customerList, setCustomerList] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     const handleGetCustomerResp = res => {
@@ -27,7 +32,9 @@ const Customers = ({ fetchApi, setFetchApiInfo }) => {
       fetchApi(false);
     };
     fetchApi(true);
-    getCustomerList().then(handleGetCustomerResp).catch(handleGetCustomerErr);
+    getCustomerList()
+      .then(handleGetCustomerResp)
+      .catch(handleGetCustomerErr);
   }, [fetchApi, setFetchApiInfo]);
 
   const handleEdit = customer => {
@@ -37,13 +44,69 @@ const Customers = ({ fetchApi, setFetchApiInfo }) => {
     return editClick;
   };
 
+  const handleSearchSubmit = () => {};
+
+  const handleSearchChange = e => {
+    const searchSuccess = res => {
+      console.log(res.data);
+      setSuggestions(res.data);
+    };
+
+    const searchErr = () => {};
+    searchCustomer(e.target.value)
+      .then(searchSuccess)
+      .catch(searchErr);
+  };
+
+  const searchComponent = (
+    <div className={classes.inputsTop}>
+      <div className={classes.searchTab}>
+        <Autocomplete
+          id='customer search-item-search'
+          getOptionLabel={option => `${option.item.id}-${option.item.itemName}`}
+          options={suggestions}
+          onChange={handleSearchSubmit}
+          getOptionDisabled={opt => opt.detail}
+          disabledItemsFocusable
+          // loading={fetchItems}
+          // onFocus={handleFocus}
+          renderInput={params => (
+            <TextField
+              autoFocus
+              {...params}
+              label='Enter an Item Code, Item Name or Item Id'
+              noOptionsText={'No items found'}
+              variant='outlined'
+              onChange={handleSearchChange}
+              // InputProps={{
+              //   ...params.InputProps,
+              //   startAdornment: <SearchIcon />,
+              //   endAdornment: (
+              //     <React.Fragment>
+              //       {fetchItems && (
+              //         <CircularProgress color='inherit' size={20} />
+              //       )}
+              //       {params.InputProps.endAdornment}
+              //     </React.Fragment>
+              //   ),
+              // }}
+            />
+          )}
+        />
+      </div>
+    </div>
+  );
+
   return (
-    <TableBuilder
-      tableData={customerList}
-      tableHeaders={getCustomerTableHeaders}
-      handleEdit={handleEdit}
-      title={'Customers'}
-    />
+    <Fragment>
+      {searchComponent}
+      <TableBuilder
+        tableData={customerList}
+        tableHeaders={getCustomerTableHeaders}
+        handleEdit={handleEdit}
+        title={'Customers'}
+      />
+    </Fragment>
   );
 };
 
