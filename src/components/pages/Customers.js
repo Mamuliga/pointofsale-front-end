7720e6ea11066546cd509f8682a3aa9e02a2241e
Marjan Mukram <marjanmukram8@gmail.com>
2020-07-01 06:34:27 +0530

    customer search component

--- src/components/pages/Customers.js
+++ src/components/pages/Customers.js
@@ -6,13 +6,15 @@ import { getCustomerTableHeaders } from '../../utilities/helpers/tableHelpers.js
 import { getCustomerList, searchCustomer } from '../../http/customerApi';
 import { fetchApi, setFetchApiInfo } from '../../store/actions/globalAction.js';
 import useStyles from '../../styles/useStyles.js';
-import { TextField } from '@material-ui/core';
+import { TextField, CircularProgress } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const Customers = ({ fetchApi, setFetchApiInfo }) => {
   const { location, push } = useHistory();
   const [customerList, setCustomerList] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
+  const [fetchCustomers, setFetchCustomers] = useState(false);
   const classes = useStyles();
 
   useEffect(() => {
@@ -48,11 +50,18 @@ const Customers = ({ fetchApi, setFetchApiInfo }) => {
 
   const handleSearchChange = e => {
     const searchSuccess = res => {
+      setFetchCustomers(false);
       console.log(res.data);
-      setSuggestions(res.data);
+      if (Array.isArray(res.data)) {
+        setSuggestions(res.data);
+      }
     };
 
-    const searchErr = () => {};
+    const searchErr = () => {
+      setFetchApiInfo({ type: 'error', message: 'Unable to search customers' });
+      setFetchCustomers(false);
+    };
+    setFetchCustomers(true);
     searchCustomer(e.target.value)
       .then(searchSuccess)
       .catch(searchErr);
@@ -63,33 +72,30 @@ const Customers = ({ fetchApi, setFetchApiInfo }) => {
       <div className={classes.searchTab}>
         <Autocomplete
           id='customer search-item-search'
-          getOptionLabel={option => `${option.item.id}-${option.item.itemName}`}
+          getOptionLabel={option => `${option.firstName}-${option.lastName}`}
           options={suggestions}
           onChange={handleSearchSubmit}
-          getOptionDisabled={opt => opt.detail}
-          disabledItemsFocusable
-          // loading={fetchItems}
-          // onFocus={handleFocus}
+          loading={fetchCustomers}
           renderInput={params => (
             <TextField
               autoFocus
               {...params}
-              label='Enter an Item Code, Item Name or Item Id'
-              noOptionsText={'No items found'}
+              label='Enter a Ccustomer name or customer Id'
+              noOptionsText={'No customers found'}
               variant='outlined'
               onChange={handleSearchChange}
-              // InputProps={{
-              //   ...params.InputProps,
-              //   startAdornment: <SearchIcon />,
-              //   endAdornment: (
-              //     <React.Fragment>
-              //       {fetchItems && (
-              //         <CircularProgress color='inherit' size={20} />
-              //       )}
-              //       {params.InputProps.endAdornment}
-              //     </React.Fragment>
-              //   ),
-              // }}
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: <SearchIcon />,
+                endAdornment: (
+                  <React.Fragment>
+                    {fetchCustomers && (
+                      <CircularProgress color='inherit' size={20} />
+                    )}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
             />
           )}
         />
